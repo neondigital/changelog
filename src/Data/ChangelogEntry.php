e2c4986d422eaea4e6d56687fc42626c947ebac8
GitHub <action@github.com>
2025-11-10 10:59:08 +0000

    Fixes

--- src/Data/ChangelogEntry.php
+++ src/Data/ChangelogEntry.php
@@ -5,6 +5,7 @@ namespace Neondigital\Changelog\Data;
 use Carbon\Carbon;
 use Illuminate\Support\Collection;
 use Spatie\LaravelData\Data;
+use Spatie\LaravelData\DataCollection;
 
 class ChangelogEntry extends Data
 {
@@ -12,8 +13,19 @@ class ChangelogEntry extends Data
         public string $filename,
         public Carbon $date,
         public string $title,
-        /** @var Collection<ChangelogEntryChange> */
-        public Collection $changes,
+        /** @var DataCollection<ChangelogEntryChange> */
+        public DataCollection $changes,
         public ?string $body = null,
     ) {}
+
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            filename: $data['filename'],
+            date: $data['date'],
+            title: $data['title'],
+            changes: ChangelogEntryChange::collection($data['changes'] ?? []),
+            body: $data['body'] ?? null,
+        );
+    }
 }
